6d3523dd57769571a0824d0344d29aeac89bbe14
Ubuntu <ubuntu@ip-172-31-88-6.ec2.internal>
2019-07-30 07:41:33 +0000

    Created the contact form with email validation

--- ecommerce/views.py
+++ ecommerce/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .forms import ContactForm
 
 
 def home_page(request):
@@ -19,8 +20,12 @@ def about_page(request):
     
     
 def contact_page(request):
+    contact_form = ContactForm(request.POST or None)
     context = {
         "title": "Contact page",
-        "content": "This will be the content of contact"
+        "content": "This will be the content of contact",
+        "form": contact_form,
     }
+    if contact_form.is_valid():
+        print(contact_form.cleaned_data)
     return render(request, "contact_page.html", context)    
