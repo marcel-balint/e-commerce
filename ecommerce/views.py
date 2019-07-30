3941d538d46a621c90e4cbb9148a84f3ffad53b7
Ubuntu <ubuntu@ip-172-31-88-6.ec2.internal>
2019-07-30 07:16:49 +0000

    Created three files which inherits from 'base.html'

--- ecommerce/views.py
+++ ecommerce/views.py
@@ -3,4 +3,24 @@ from django.shortcuts import render
 
 
 def home_page(request):
-    return render(request, "home_page.html", {})
+    context = {
+        "title": "Home page",
+        "content": "This will be the content"
+    }
+    return render(request, "home_page.html", context)
+    
+    
+def about_page(request):
+    context = {
+        "title": "About page",
+        "content": "This will be the content of about"
+    }
+    return render(request, "about_page.html", context)    
+    
+    
+def contact_page(request):
+    context = {
+        "title": "Contact page",
+        "content": "This will be the content of contact"
+    }
+    return render(request, "contact_page.html", context)    
