3254965e9ee686885288c3be343b046dfba7d6bd
Ubuntu <ubuntu@ip-172-31-88-6.ec2.internal>
2019-08-01 11:11:45 +0000

    Added logic to calculate the price of the cart items

--- cart/views.py
+++ cart/views.py
@@ -3,14 +3,11 @@ from .models import Cart
 
 
 def cart_home(request):
-    cart_id = request.session.get("cart_id", None)
-    qs = Cart.objects.filter(id=cart_id)
-    if qs.count() == 1:
-        cart_obj = qs.first()
-        if request.user.is_authenticated() and cart_obj.user is None:
-            cart_obj.user = request.user
-            cart_obj.save()
-    else:
-        cart_obj = Cart.objects.new_cart(user=request.user)
-        request.session["cart_id"] = cart_obj.id
+    cart_obj, new_obj = Cart.objects.new_or_get(request)
+    products = cart_obj.products.all()
+    total = 0
+    for x in products:
+        total += x.price
+    cart_obj.total = total
+    cart_obj.save()
     return render(request, "cart/home.html", {})
