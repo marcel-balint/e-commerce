b2252742821a3b4116d23fc7f484db31c982c88a
Ubuntu <ubuntu@ip-172-31-88-6.ec2.internal>
2019-08-01 08:21:50 +0000

    Modified the view to associates a cart to a user if the user don't have one

--- cart/views.py
+++ cart/views.py
@@ -1,17 +1,15 @@
 from django.shortcuts import render
 from .models import Cart
 
-def cart_create(user=None):
-    cart_obj = Cart.objects.create(user=None)
-    return cart_obj
 
 def cart_home(request):
-    request.session["cart_id"] = "12"
     cart_id = request.session.get("cart_id", None)
     qs = Cart.objects.filter(id=cart_id)
     if qs.count() == 1:
-        print("Cart ID exist")
         cart_obj = qs.first()
+        if request.user.is_authenticated() and cart_obj.user is None:
+            cart_obj.user = request.user
+            cart_obj.save()
     else:
         cart_obj = Cart.objects.new_cart(user=request.user)
         request.session["cart_id"] = cart_obj.id
