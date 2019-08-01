55447c0bd61dfe4b10379ce1ab954a0d3a26f96b
Ubuntu <ubuntu@ip-172-31-88-6.ec2.internal>
2019-08-01 07:58:57 +0000

    Created the cart view

--- cart/views.py
+++ cart/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
+from .models import Cart
 
+def cart_create(user=None):
+    cart_obj = Cart.objects.create(user=None)
+    return cart_obj
 
 def cart_home(request):
-    print(request.session)
-    return render(request, "cart/home.html")
+    request.session["cart_id"] = "12"
+    cart_id = request.session.get("cart_id", None)
+    qs = Cart.objects.filter(id=cart_id)
+    if qs.count() == 1:
+        print("Cart ID exist")
+        cart_obj = qs.first()
+    else:
+        cart_obj = Cart.objects.new_cart(user=request.user)
+        request.session["cart_id"] = cart_obj.id
+    return render(request, "cart/home.html", {})
