3428143aa6910d9e7c8685c4b6c94d6538affff5
Ubuntu <ubuntu@ip-172-31-88-6.ec2.internal>
2019-08-01 12:38:59 +0000

    Created the functionality to update the cart

--- products/views.py
+++ products/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import Http404
 from .models import Product
+#
+from django.views.generic import DetailView
+from cart.models import Cart
 
 
 def product_list(request):
@@ -24,3 +27,10 @@ def product_detail(request, pk):
     return render(request, "products/product_detail.html", context)    
     
     
+class ProductDetailSlugView(DetailView):
+    template = "products/product_detail.html"
+    def get_context_data(self, *args, **kwargs):
+        context = super(ProductDetailSlugView, self).get_context_data(*args, **kwargs)
+        cart_obj, new_obj = Cart.objects.new_or_get(self.request)
+        context['cart'] = cart_obj
+        return context
