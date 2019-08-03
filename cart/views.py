17a5b351473324cfa31071f669eeb6d27656aa07
Ubuntu <ubuntu@ip-172-31-88-6.ec2.internal>
2019-08-03 12:03:40 +0000

    Redirect a user to login for checkout

--- cart/views.py
+++ cart/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from .models import Cart
 from products.models import Product
 from orders.models import Order
+from accounts.forms import LoginForm
+from billing.models import BillingProfile
 
 
 def cart_home(request):
@@ -32,6 +34,18 @@ def checkout_home(request):
         return redirect("cart:home")
     else:
         order_obj, new_order_obj = Order.objects.get_or_create(cart=cart_obj)
-    return render(request, "cart/checkout.html", {"object": order_obj})
+    user = request.user
+    billing_profile = None
+    login_form = LoginForm()
+    if user.is_authenticated():
+        billing_profile, billing_profile_created = BillingProfile.objects.get_or_create(
+                                                        user=user)
+    
+    context = {
+        "object": order_obj,
+        "billing_profile": billing_profile,
+        "login_form": login_form,
+    }    
+    return render(request, "cart/checkout.html", context)
 
 
