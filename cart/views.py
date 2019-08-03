f4eb9ae2b936a20e935b86d55ed9a266b7e9c61c
Ubuntu <ubuntu@ip-172-31-88-6.ec2.internal>
2019-08-03 08:01:00 +0000

    Created the checkout view

--- cart/views.py
+++ cart/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from .models import Cart
 from products.models import Product
+from orders.models import Order
 
 
 def cart_home(request):
@@ -22,6 +23,15 @@ def cart_update(request):
             cart_obj.products.remove(product_obj)
         request.session['cart_items'] = cart_obj.products.count()
     return redirect("cart:home")
-    
+
+
+def checkout_home(request):
+    cart_obj, cart_created = Cart.objects.new_or_get(request)
+    order_obj = None
+    if cart_created or cart_obj.products.count() == 0:
+        return redirect("cart:home")
+    else:
+        order_obj, new_order_obj = Order.objects.get_or_create(cart=cart_obj)
+    return render(request, "cart/checkout.html", {"object": order_obj})
 
 
