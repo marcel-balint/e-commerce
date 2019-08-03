a9ab17ac2c302e6c59d87e909a0bfa9188fcecfb
Ubuntu <ubuntu@ip-172-31-88-6.ec2.internal>
2019-08-03 08:37:45 +0000

    Fixd the 'type error'

--- orders/models.py
+++ orders/models.py
@@ -3,6 +3,7 @@ from django.db import models
 from cart.models import Cart
 from ecommerce.utils import unique_order_id_generator
 from django.db.models.signals import pre_save, post_save
+import math
 
 
 
@@ -27,8 +28,9 @@ class Order(models.Model):
     def update_total(self):
         cart_total = self.cart.total
         shipping_total = self.shipping_total
-        new_total = cart_total + shipping_total
-        self.total = new_total
+        new_total = math.fsum([cart_total, shipping_total])
+        formatted_total = format(new_total, '.2f')
+        self.total = formatted_total
         self.save()
         return new_total
 
