7f2ea937792af36fa807651350820fb3f77b3a60
Ubuntu <ubuntu@ip-172-31-88-6.ec2.internal>
2019-08-19 09:35:02 +0000

    Formatted the 'billing' app code to conform to the PEP 8 style guide

--- billing/models.py
+++ billing/models.py
@@ -17,14 +17,14 @@ class BillingProfileManager(models.Manager):
         created = False
         obj = None
         if user.is_authenticated():
-            #logged in user checkout; remember payment
+            # logged in user checkout; remember payment
             obj, created = self.model.objects.get_or_create(
-                            user=user)
+                user=user)
         elif guest_email_id is not None:
-            #guest user checkout; auto reloads payment 
+            # guest user checkout; auto reloads payment
             guest_email_obj = GuestEmail.objects.get(id=guest_email_id)
             obj, created = self.model.objects.get_or_create(
-                                            email=guest_email_obj.email)
+                email=guest_email_obj.email)
         else:
             pass
         return obj, created
@@ -37,55 +37,54 @@ class BillingProfile(models.Model):
     update = models.DateTimeField(auto_now_add=True)
     timestamp = models.DateTimeField(auto_now_add=True)
     customer_id = models.CharField(max_length=120, null=True, blank=True)
-    
+
     objects = BillingProfileManager()
-    
+
     def __str__(self):
         return self.email
-    
+
     def charge(self, order_obj, card=None):
         return Charge.objects.do(self, order_obj, card)
-    
-    
+
     def get_cards(self):
         return self.card_set.all()
-        
-    
+
     def get_payment_method_url(self):
         return reverse('billing-payment-method')
-        
-        
-    #property of the model
+
+    # property of the model
     @property
-    def has_card(self): # instance.has_card
+    def has_card(self):  # instance.has_card
         card_qs = self.get_cards()
-        return card_qs.exists() # True or False 
-        
-        
+        return card_qs.exists()  # True or False
+
     @property
     def default_card(self):
         default_cards = self.get_cards().filter(default=True)
         if default_cards.exists():
             return default_cards.first()
-        return None    
+        return None
 
-    
+
 def billing_profile_created_receiver(sender, instance, *args, **kwargs):
     if not instance.customer_id and instance.email:
         print("ACTUAL API REQUEST Send to stripe")
         customer = stripe.Customer.create(
-                email = instance.email
-            )
+            email=instance.email
+        )
         print(customer)
         instance.customer_id = customer.id
 
-pre_save.connect(billing_profile_created_receiver, sender=BillingProfile)    
-    
-    
+
+pre_save.connect(billing_profile_created_receiver, sender=BillingProfile)
+
+
 def user_created_receiver(sender, instance, created, *args, **kwargs):
     if created and instance.email:
-        BillingProfile.objects.get_or_create(user=instance, email=instance.email)
-        
+        BillingProfile.objects.get_or_create(
+            user=instance, email=instance.email)
+
+
 post_save.connect(user_created_receiver, sender=User)
 
 
@@ -95,17 +94,18 @@ class CardManager(models.Manager):
             customer = stripe.Customer.retrieve(billing_profile.customer_id)
             stripe_card_response = customer.sources.create(source=token)
             new_card = self.model(
-                    billing_profile=billing_profile,
-                    stripe_id = stripe_card_response.id,
-                    brand = stripe_card_response.brand,
-                    country = stripe_card_response.country,
-                    exp_month = stripe_card_response.exp_month,
-                    exp_year = stripe_card_response.exp_year,
-                    last4 = stripe_card_response.last4
-                )
+                billing_profile=billing_profile,
+                stripe_id=stripe_card_response.id,
+                brand=stripe_card_response.brand,
+                country=stripe_card_response.country,
+                exp_month=stripe_card_response.exp_month,
+                exp_year=stripe_card_response.exp_year,
+                last4=stripe_card_response.last4
+            )
             new_card.save()
             return new_card
         return None
+
 
 class Card(models.Model):
     billing_profile = models.ForeignKey(BillingProfile)
@@ -114,64 +114,66 @@ class Card(models.Model):
     country = models.CharField(max_length=20, null=True, blank=True)
     exp_month = models.IntegerField(null=True, blank=True)
     exp_year = models.IntegerField(null=True, blank=True)
-    last4  = models.CharField(max_length=4, null=True, blank=True)
+    last4 = models.CharField(max_length=4, null=True, blank=True)
     default = models.BooleanField(default=True)
 
     objects = CardManager()
-    
+
     def __str__(self):
         return "{} {}".format(self.brand, self.last4)
-    
+
 
 def new_card_post_save_receiver(sender, instance, created, *args, **kwargs):
     if instance.default:
         billing_profile = instance.billing_profile
-        qs = Card.objects.filter(billing_profile=billing_profile).exclude(pk=instance.pk)
+        qs = Card.objects.filter(
+            billing_profile=billing_profile).exclude(
+            pk=instance.pk)
         qs.update(default=False)
 
 
 post_save.connect(new_card_post_save_receiver, sender=Card)
 
-    
+
 class ChargeManager(models.Manager):
-    #Create a charge based on Charge model
-    def do(self, billing_profile, order_obj, card=None): 
+    # Create a charge based on Charge model
+    def do(self, billing_profile, order_obj, card=None):
         card_obj = card
         if card_obj is None:
-            cards = billing_profile.card_set.filter(default=True) 
+            cards = billing_profile.card_set.filter(default=True)
             if cards.exists():
                 card_obj = cards.first()
         if card_obj is None:
             return False, "No cards available"
         c = stripe.Charge.create(
-              amount = int(order_obj.total * 100), 
-              currency = "usd",
-              customer =  billing_profile.customer_id,
-              source = card_obj.stripe_id,
-              metadata={"order_id":order_obj.order_id},
-            )
+            amount=int(order_obj.total * 100),
+            currency="usd",
+            customer=billing_profile.customer_id,
+            source=card_obj.stripe_id,
+            metadata={"order_id": order_obj.order_id},
+        )
         new_charge_obj = self.model(
-                billing_profile = billing_profile,
-                stripe_id = c.id,
-                paid = c.paid,
-                refunded = c.refunded,
-                outcome = c.outcome,
-                outcome_type = c.outcome['type'],
-                seller_message = c.outcome.get('seller_message'),
-                risk_level = c.outcome.get('risk_level'),
+            billing_profile=billing_profile,
+            stripe_id=c.id,
+            paid=c.paid,
+            refunded=c.refunded,
+            outcome=c.outcome,
+            outcome_type=c.outcome['type'],
+            seller_message=c.outcome.get('seller_message'),
+            risk_level=c.outcome.get('risk_level'),
         )
         new_charge_obj.save()
-        return new_charge_obj.paid, new_charge_obj.seller_message        
-        
+        return new_charge_obj.paid, new_charge_obj.seller_message
+
 
 class Charge(models.Model):
-    billing_profile         = models.ForeignKey(BillingProfile)
-    stripe_id               = models.CharField(max_length=120)
-    paid                    = models.BooleanField(default=False)
-    refunded                = models.BooleanField(default=False)
-    outcome                 = models.TextField(null=True, blank=True)
-    outcome_type            = models.CharField(max_length=120, null=True, blank=True)
-    seller_message          = models.CharField(max_length=120, null=True, blank=True)
-    risk_level              = models.CharField(max_length=120, null=True, blank=True)
+    billing_profile = models.ForeignKey(BillingProfile)
+    stripe_id = models.CharField(max_length=120)
+    paid = models.BooleanField(default=False)
+    refunded = models.BooleanField(default=False)
+    outcome = models.TextField(null=True, blank=True)
+    outcome_type = models.CharField(max_length=120, null=True, blank=True)
+    seller_message = models.CharField(max_length=120, null=True, blank=True)
+    risk_level = models.CharField(max_length=120, null=True, blank=True)
 
-    objects = ChargeManager()        
+    objects = ChargeManager()
--- billing/views.py
+++ billing/views.py
@@ -12,24 +12,28 @@ stripe.api_key = STRIPE_SECRET_KEY
 
 
 def payment_method_view(request):
-    billing_profile, billing_profile_created = BillingProfile.objects.new_or_get(request)
+    billing_profile, billing_profile_created = BillingProfile.objects.new_or_get(
+        request)
     if not billing_profile:
         return redirect("/cart")
     next_url = None
     next_ = request.GET.get('next')
     if is_safe_url(next_, request.get_host()):
         next_url = next_
-    return render(request, 'billing/payment-method.html', {"publish_key": STRIPE_PUB_KEY, "next_url": next_url})
-    
-    
+    return render(request, 'billing/payment-method.html',
+                  {"publish_key": STRIPE_PUB_KEY, "next_url": next_url})
+
+
 def payment_method_createview(request):
     if request.method == "POST" and request.is_ajax():
-        billing_profile, billing_profile_created = BillingProfile.objects.new_or_get(request)
+        billing_profile, billing_profile_created = BillingProfile.objects.new_or_get(
+            request)
         if not billing_profile:
-            return HttpResponse({"message": "Cannot find this user"}, status=401)
+            return HttpResponse(
+                {"message": "Cannot find this user"}, status=401)
         token = request.POST.get("token")
         if token is not None:
-            #Create a new customer card source based on token
-             new_card_obj = Card.objects.add_new(billing_profile, token)
+            # Create a new customer card source based on token
+            new_card_obj = Card.objects.add_new(billing_profile, token)
         return JsonResponse({"message": "Success! Your card was added."})
     return HttpResponse("error", status=401)
