6039dac59846e1724c1991761bd9e521fb3c4f7b
Ubuntu <ubuntu@ip-172-31-88-6.ec2.internal>
2019-08-20 12:49:34 +0000

    Modified settings for S3 functionality

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -140,19 +140,25 @@ AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=94608000'
 }
 
+
 AWS_STORAGE_BUCKET_NAME = 'project-ecommerce'
 AWS_S3_REGION_NAME = 'eu-west-1'
 AWS_ACCESS_KEY_ID = os.environ.get("AWS_SECRET_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_LOCATION = 'static'
+STATICFILES_STORAGE = 'custom_storages.StaticStorage'
 
+MEDIAFILES_LOCATION = 'media'
+DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 MEDIA_URL = '/media/'
+MEDIA_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, MEDIAFILES_LOCATION)
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STRIPE_PUB_KEY = os.getenv('STRIPE_PUB_KEY')
